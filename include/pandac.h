d6a35fd8fdcfda368f484947d7029b94c145ebad
a3a256 <81564718+a3a256@users.noreply.github.com>
2024-04-15 23:21:58 +0100

    + operator overload

--- include/pandac.h
+++ include/pandac.h
@@ -68,6 +68,7 @@ class Series{
         // start testing and implementing cout overload operator for Series class
         friend std::ostream& operator<<(std::ostream& os, const Series& dt);
         Series operator+(std::string const &obj);
+        Series operator+(int const &obj);
 
 
     private:
@@ -154,6 +155,18 @@ Series Series::operator+(std::string const& obj){
     std::vector<std::string> vals;
     for(i=0; i<values.size(); i++){
         vals.push_back(values[i].line+obj);
+    }
+    Series n;
+    n.to_series(vals, col_name);
+
+    return n;
+}
+
+Series Series::operator+(int const &obj){
+    int i;
+    std::vector<std::string> vals;
+    for(i=0; i<values.size(); i++){
+        vals.push_back(std::to_string(values[i].num+obj));
     }
     Series n;
     n.to_series(vals, col_name);
