6fa44307e8db27a94a9d4f393270be7ccbf21578
a3a256 <81564718+a3a256@users.noreply.github.com>
2024-04-20 04:03:22 +0100

    += overload operator

--- include/pandac.h
+++ include/pandac.h
@@ -70,6 +70,9 @@ class Series{
         Series operator+(std::string const &obj);
         Series operator+(int const &obj);
         Series operator+(float const&obj);
+        Series operator+=(std::string const &obj);
+        Series operator+=(int const &obj);
+        Series operator+=(float const&obj);
 
 
     private:
@@ -163,6 +166,18 @@ Series Series::operator+(std::string const& obj){
     return n;
 }
 
+Series Series::operator+=(std::string const& obj){
+    int i;
+    std::vector<std::string> vals;
+    for(i=0; i<values.size(); i++){
+        vals.push_back(values[i].line+obj);
+    }
+    Series n;
+    n.to_series(vals, col_name);
+
+    return n;
+}
+
 Series Series::operator+(int const &obj){
     int i;
     std::vector<std::string> vals;
@@ -175,7 +190,31 @@ Series Series::operator+(int const &obj){
     return n;
 }
 
+Series Series::operator+=(int const &obj){
+    int i;
+    std::vector<std::string> vals;
+    for(i=0; i<values.size(); i++){
+        vals.push_back(std::to_string((int)values[i].num+obj));
+    }
+    Series n;
+    n.to_series(vals, col_name);
+
+    return n;
+}
+
 Series Series::operator+(float const &obj){
+    int i;
+    std::vector<std::string> vals;
+    for(i=0; i<values.size(); i++){
+        vals.push_back(std::to_string(values[i].num+obj));
+    }
+    Series n;
+    n.to_series(vals, col_name);
+
+    return n;
+}
+
+Series Series::operator+=(float const &obj){
     int i;
     std::vector<std::string> vals;
     for(i=0; i<values.size(); i++){
